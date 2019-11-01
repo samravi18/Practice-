17ef3c67c6e59b72f85e2105d7850000e1ef9f43
Samhita Ravi <sar160@pitt.edu>
2019-11-01 15:22:06 -0400

    Added code to try out last component of project

--- src/analysis.R
+++ src/analysis.R
@@ -7,3 +7,10 @@ barplot(tigerTable, ylab = "Frequency", cex.names = 0.5, las = 2)
 dev.off()
 
 
+lion <- read.csv(url("http://www.zoology.ubc.ca/~schluter/WhitlockSchluter/wp-content/data/chapter17/chap17e1LionNoses.csv"))
+head(lion)
+lionRegression <-lm(ageInYears ~ proportionBlack,data=lion)
+sumary(lionRegression)
+
+out<-capture.output(summary(lionRegression))
+cat("Results", out, file="../results/summary.txt", sep="/n", append=FALSE)
